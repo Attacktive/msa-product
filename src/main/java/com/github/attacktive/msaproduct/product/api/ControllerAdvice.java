d3f1d897381d2b56c777cc7ef7aa7d959a0beafc
Pantera <w.jung@okestro.com>
2024-06-04 12:46:01 +0900

    feature: give minimal response on error

--- src/main/java/com/github/attacktive/msaproduct/product/api/ControllerAdvice.java
+++ src/main/java/com/github/attacktive/msaproduct/product/api/ControllerAdvice.java
@@ -3,6 +3,7 @@ package com.github.attacktive.msaproduct.product.api;
 import javax.xml.bind.ValidationException;
 
 import com.github.attacktive.msaproduct.product.api.exception.NoSuchProductException;
+import com.github.attacktive.msaproduct.product.api.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageConversionException;
@@ -15,27 +16,35 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ControllerAdvice {
 	@ExceptionHandler(HttpMessageConversionException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public void handleHttpMessageConversionException(HttpMessageConversionException httpMessageConversionException) {
+	public ErrorResponse handleHttpMessageConversionException(HttpMessageConversionException httpMessageConversionException) {
 		var message = "Couldn't parse the request body: %s".formatted(httpMessageConversionException.getMessage());
 		log.error(message, httpMessageConversionException);
+
+		return new ErrorResponse(HttpStatus.BAD_REQUEST, message);
 	}
 
 	@ExceptionHandler(ValidationException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public void handleValidationException(ValidationException validationException) {
+	public ErrorResponse handleValidationException(ValidationException validationException) {
 		var message = "Failed to validate the request: %s".formatted(validationException.getMessage());
 		log.error(message, validationException);
+
+		return new ErrorResponse(HttpStatus.BAD_REQUEST, message);
 	}
 
 	@ExceptionHandler(NoSuchProductException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public void handleNoSuchProductException(NoSuchProductException noSuchProductException) {
+	public ErrorResponse handleNoSuchProductException(NoSuchProductException noSuchProductException) {
 		log.error(noSuchProductException.getMessage(), noSuchProductException);
+
+		return new ErrorResponse(HttpStatus.NOT_FOUND, noSuchProductException.getMessage());
 	}
 
 	@ExceptionHandler(Throwable.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public void handleThrowable(Throwable throwable) {
+	public ErrorResponse handleThrowable(Throwable throwable) {
 		log.error(throwable.getMessage(), throwable);
+
+		return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, throwable.getMessage());
 	}
 }
